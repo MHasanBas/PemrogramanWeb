2176ad11ffd5cf98727b7709aa016deb5aa95fd2
MHasanBas <hasanbasr054@gmail.com>
2024-03-23 23:01:54 +0700

    PHP 3 (PRAKTIKUM 1 - Indexed Array)

---
+++ pertemuan6/praktik_php/array_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Array terindeks</h2>
+    <?php
+    // Mendefinisikan array $Listdosen yang berisi nama-nama dosen
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    // Menampilkan elemen array dengan indeks tertentu
+    echo $Listdosen[2] . "<br>"; // Menampilkan nama dosen indeks ke-2
+    echo $Listdosen[0] . "<br>"; // Menampilkan nama dosen indeks ke-0
+    echo $Listdosen[1] . "<br>"; // Menampilkan nama dosen indeks ke-1
+    ?>
+</body>
+
+</html>
